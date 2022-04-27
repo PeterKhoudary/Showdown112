ec8babc0088035d0ccc9c5bb50baa1b501b3bdd0
PeterKhoudary <peter.m.khoudary@gmail.com>
2022-04-27 07:17:25 -0400

    little evaluation refinement

--- minmax.py
+++ minmax.py
@@ -212,6 +212,9 @@ def stateEvaluation(newState):
                 score -= 50
             else:
                 score -= 10
+    for playerMon in playerTeam:
+        playerPercent = player.currentHP / player.finalStats["HP"]
+        score += (100 / playerPercent)
     return score
 
 ###############################################################################
